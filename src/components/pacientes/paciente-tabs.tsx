d873f6a44538d1239a7923304144ad9d7e01bf79
Guilherme Vianna <mataveli91@gmail.com>
2026-08-17 19:06:25 -0300

    Corrige responsividade horizontal das abas do paciente em telas pequenas

--- src/components/pacientes/paciente-tabs.tsx
+++ src/components/pacientes/paciente-tabs.tsx
@@ -12,13 +12,19 @@ export function PacienteTabs({
 }) {
   return (
     <Tabs defaultValue={defaultValue} className="gap-4">
-      <TabsList className="w-full sm:w-fit">
-        {tabs.map((tab) => (
-          <TabsTrigger key={tab.value} value={tab.value}>
-            {tab.label}
-          </TabsTrigger>
-        ))}
-      </TabsList>
+      <div className="-mx-4 overflow-x-auto px-4 md:-mx-6 md:px-6">
+        <TabsList className="w-max">
+          {tabs.map((tab) => (
+            <TabsTrigger
+              key={tab.value}
+              value={tab.value}
+              className="flex-none whitespace-nowrap"
+            >
+              {tab.label}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </div>
       {tabs.map((tab) => (
         <TabsContent key={tab.value} value={tab.value} className="flex flex-col gap-4">
           {tab.action && <div className="flex justify-end">{tab.action}</div>}
